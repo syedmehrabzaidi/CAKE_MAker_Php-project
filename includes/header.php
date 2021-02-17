660d17fdb9a9e6b57fb42b5f52abe3863b266b55
Mehrab Zaidi <45766880+syedmehrabzaidi@users.noreply.github.com>
2021-02-18 02:39:01 +0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,82 @@
+<head>
+
+<style>
+#logo1{
+  border-radius: 40%;
+  margin-left: 10px;
+  width: 70px;
+  height: 50px;
+  margin-top: -12px;
+
+  margin-bottom: -20px;
+}
+</style>
+
+
+</head>
+
+<header >
+
+	
+	<div class="nav_toggle bg-danger" onclick="toggle_class();">
+		
+		<span class="toggle_icon"></span>
+		
+	</div>
+	
+	<div onclick="remove_class()">
+	
+	<div class="main_nav">
+		
+		<a href="index.php">
+		<img width="110px" id="logo1" src="image/logo3.jpg">
+		</a>
+		
+		<ul class="default_links">
+			
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Cake Menu</a></li>
+			<li><a href="reservation.php">Reserve Cakes</a></li>
+			<li><a href="contactus.php">Our Shop</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="basket.php">Order</a></li>
+			
+		</ul>
+		
+		<p class="clear"></p>
+		
+	</div>
+	
+	<p class="clear"></p>
+	
+	</div>
+	
+</header>
+
+<div class="responive_nav">
+	
+	<div class="nav_section_img">
+		
+		<div class="nav_section_div">
+			
+			<h3>La delicacy</h3>
+			
+		</div>
+		
+	</div>
+	
+	<div class="nav_section">
+		
+		<ul>
+			
+			<li><a href="index.php"><span class="home">Home</span></a></li>
+			<li><a href="menu.php"><span class="menu">Menu</span></a></li>
+			<li><a href="reservation.php"><span class="reserve">Book Table</span></a></li>
+			<li><a href="gallery.php"><span class="gallery">Gallery</span></a></li>
+			<li><a href="basket.php"><span class="order">Order</span></a></li>
+			
+		</ul>
+		
+	</div>
+	
+</div>
